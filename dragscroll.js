7e8edf79ab51d3dc1239bdbd42d23b3c4ccd74c6
Павел Гвоздь <gvozdb@users.noreply.github.com>
2018-11-21 13:58:12 +0600

    Added support touch devices

--- dragscroll.js
+++ dragscroll.js
@@ -1,9 +1,9 @@
 /**
- * @fileoverview dragscroll - scroll area by dragging
- * @version 0.0.8
- * 
- * @license MIT, see http://github.com/asvd/dragscroll
- * @copyright 2015 asvd <heliosframework@gmail.com> 
+ * @fileoverview dragscroll - scroll area by dragging and touching
+ * @version 1.0.0
+ *
+ * @license MIT, see http://github.com/gvozdb/dragscroll
+ * @copyright 2018 asvd <heliosframework@gmail.com>, gvozdb <pavelgvozdb@yandex.ru>
  */
 
 
@@ -18,69 +18,102 @@
 }(this, function (exports) {
     var _window = window;
     var _document = document;
-    var mousemove = 'mousemove';
-    var mouseup = 'mouseup';
-    var mousedown = 'mousedown';
+    var selector = '.js-dragscroll';
+    var mousemove = ['mousemove', 'touchmove'];
+    var mouseup = ['mouseup', 'touchend'];
+    var mousedown = ['mousedown', 'touchstart'];
     var EventListener = 'EventListener';
-    var addEventListener = 'add'+EventListener;
-    var removeEventListener = 'remove'+EventListener;
+    var addEventListener = 'add' + EventListener;
+    var removeEventListener = 'remove' + EventListener;
     var newScrollX, newScrollY;
 
     var dragged = [];
-    var reset = function(i, el) {
+    var reset = function (i, n, el) {
         for (i = 0; i < dragged.length;) {
             el = dragged[i++];
             el = el.container || el;
-            el[removeEventListener](mousedown, el.md, 0);
-            _window[removeEventListener](mouseup, el.mu, 0);
-            _window[removeEventListener](mousemove, el.mm, 0);
+            // console.log('el', el);
+
+            for (n = 0; n < mousedown['length']; ++n) {
+                el[removeEventListener](mousedown[n], el.md, 0);
+            }
+            for (n = 0; n < mouseup['length']; ++n) {
+                _window[removeEventListener](mouseup[n], el.mu, 0);
+            }
+            for (n = 0; n < mousemove['length']; ++n) {
+                _window[removeEventListener](mousemove[n], el.mm, 0);
+            }
         }
 
         // cloning into array since HTMLCollection is updated dynamically
-        dragged = [].slice.call(_document.getElementsByClassName('dragscroll'));
+        dragged = _document.querySelectorAll(selector);
         for (i = 0; i < dragged.length;) {
-            (function(el, lastClientX, lastClientY, pushed, scroller, cont){
-                (cont = el.container || el)[addEventListener](
-                    mousedown,
-                    cont.md = function(e) {
-                        if (!el.hasAttribute('nochilddrag') ||
-                            _document.elementFromPoint(
-                                e.pageX, e.pageY
-                            ) == cont
-                        ) {
-                            pushed = 1;
-                            lastClientX = e.clientX;
-                            lastClientY = e.clientY;
+            (function (el, lastClientX, lastClientY, pushed, scroller, cont) {
+                console.log('cont', cont);
 
-                            e.preventDefault();
-                        }
-                    }, 0
-                );
+                for (n = 0; n < mousedown['length']; ++n) {
+                    (cont = el.container || el)[addEventListener](mousedown[n],
+                        cont.md = function (e) {
+                            console.log('e', e);
+                            console.log('el', el);
 
-                _window[addEventListener](
-                    mouseup, cont.mu = function() {pushed = 0;}, 0
-                );
+                            var isTouch = (e['type'].indexOf('touch') !== -1);
+                            var pageX = isTouch ? e.touches[0]['pageX'] : e['pageX'];
+                            var pageY = isTouch ? e.touches[0]['pageY'] : e['pageY'];
 
-                _window[addEventListener](
-                    mousemove,
-                    cont.mm = function(e) {
-                        if (pushed) {
-                            (scroller = el.scroller||el).scrollLeft -=
-                                newScrollX = (- lastClientX + (lastClientX=e.clientX));
-                            scroller.scrollTop -=
-                                newScrollY = (- lastClientY + (lastClientY=e.clientY));
-                            if (el == _document.body) {
-                                (scroller = _document.documentElement).scrollLeft -= newScrollX;
-                                scroller.scrollTop -= newScrollY;
+                            if (!el.hasAttribute('nochilddrag') ||
+                                _document.elementFromPoint(
+                                    pageX, pageY
+                                ) == cont
+                            ) {
+                                var clientX = isTouch ? e.touches[0]['clientX'] : e['clientX'];
+                                var clientY = isTouch ? e.touches[0]['clientY'] : e['clientY'];
+
+                                pushed = 1;
+                                lastClientX = clientX;
+                                lastClientY = clientY;
+
+                                e.preventDefault();
                             }
-                        }
-                    }, 0
-                );
-             })(dragged[i++]);
+                        }, 0
+                    );
+                }
+
+                for (n = 0; n < mouseup['length']; ++n) {
+                    _window[addEventListener](mouseup[n],
+                        cont.mu = function () {
+                            pushed = 0;
+                        }, 0
+                    );
+                }
+
+                for (n = 0; n < mousemove['length']; ++n) {
+                    _window[addEventListener](mousemove[n],
+                        cont.mm = function (e) {
+                            if (pushed) {
+                                console.log('e', e);
+
+                                var isTouch = (e['type'].indexOf('touch') !== -1);
+                                var clientX = isTouch ? e.touches[0]['clientX'] : e['clientX'];
+                                var clientY = isTouch ? e.touches[0]['clientY'] : e['clientY'];
+
+                                (scroller = el.scroller || el).scrollLeft -=
+                                    newScrollX = (-lastClientX + (lastClientX = clientX));
+                                scroller.scrollTop -=
+                                    newScrollY = (-lastClientY + (lastClientY = clientY));
+                                if (el == _document.body) {
+                                    (scroller = _document.documentElement).scrollLeft -= newScrollX;
+                                    scroller.scrollTop -= newScrollY;
+                                }
+                            }
+                        }, 0
+                    );
+                }
+            })(dragged[i++]);
         }
     }
 
-      
+
     if (_document.readyState == 'complete') {
         reset();
     } else {
@@ -89,4 +122,3 @@
 
     exports.reset = reset;
 }));
-
